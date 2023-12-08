a36b8826604e84a4d603c70eab4e882ac7099c7a
Katherine Paucar <gaby2091@outlook.es>
2023-12-08 13:45:16 +0100

    update

--- 1-React-basic/src/list.tsx
+++ 1-React-basic/src/list.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import { Link } from "react-router-dom";
+import { useDebounce } from 'use-debounce';
 
 interface MemberEntity {
   id: string;
@@ -9,16 +10,32 @@ interface MemberEntity {
 
 export const ListPage: React.FC = () => {
   const [members, setMembers] = React.useState<MemberEntity[]>([]);
-
+  const [filter, setFilter] = React.useState<string>('lemoncode');
+  const [valueFilter] = useDebounce(filter, 1000);
+//` https://api.github.com/orgs/${filter}/members
   React.useEffect(() => {
-    fetch(`https://api.github.com/orgs/lemoncode/members`)
+    fetch(`${filter}/members`)
       .then((response) => response.json())
       .then((json) => setMembers(json));
   }, []);
+const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  console.log('value', valueFilter);
 
+}
+  
   return (
     <>
       <h2>Hello from List page</h2>+{" "}
+      <form  className="form" onSubmit={handleSubmit}>
+        <input 
+        type="text"
+        value={filter}
+        required
+        onChange={(e) => setFilter((e.target.value).toLocaleLowerCase())}/> 
+        <button >Filtrar por organización</button>
+      </form>
+
       <div className="list-user-list-container">
         <span className="list-header">Avatar</span>
         <span className="list-header">Id</span>
