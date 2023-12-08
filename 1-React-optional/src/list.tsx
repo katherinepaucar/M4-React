95a14bd84e1fedcf77c994de101593b1bf53396b
Katherine Paucar <gaby2091@outlook.es>
2023-12-08 17:40:42 +0100

    Optional

---
+++ 1-React-optional/src/list.tsx
@@ -0,0 +1,58 @@
+import React, { FormEvent } from "react";
+import { Link } from "react-router-dom";
+import { useDebounce } from 'use-debounce';
+
+interface MemberEntity {
+  id: string;
+  login: string;
+  avatar_url: string;
+}
+
+export const ListPage: React.FC = () => {
+  const [members, setMembers] = React.useState<MemberEntity[]>([]);
+  const [filter, setFilter] = React.useState<string>('lemoncode');
+  const [valueFilter] = useDebounce(filter, 1000);
+  const [sendData, setSendata] = React.useState<string>();
+//` https://api.github.com/orgs/${filter}/members
+  React.useEffect(() => {
+    fetch(`https://api.github.com/orgs/${filter}/members`)
+      .then((response) => response.json())
+      .then((res) => setMembers(res))
+      .catch((err)=> console.log(err));
+  }, [sendData]);
+
+const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  setSendata(valueFilter);
+  console.log('value', valueFilter);
+
+}
+  
+  return (
+    <>
+      <h2>Hello from List page</h2>+{" "}
+      <form  className="form" onSubmit={handleSubmit}>
+        <input 
+        type="text"
+        value={filter}
+        required
+        onChange={(e) => setFilter((e.target.value).toLocaleLowerCase())}/> 
+        <button >Filtrar por organización</button>
+      </form>
+
+      <div className="list-user-list-container">
+        <span className="list-header">Avatar</span>
+        <span className="list-header">Id</span>
+        <span className="list-header">Name</span>
+        {members && members.length> 0 && members.map((member) => (
+          <React.Fragment key={member.id}>
+            <img src={member.avatar_url} />
+            <span>{member.id}</span>
+            <Link to={`/detail/${member.login}`}>{member.login}</Link>
+          </React.Fragment>
+        ))}
+      </div>
+      <Link to="/detail">Navigate to detail page</Link>
+    </>
+  );
+};
