37a282ebddf83812857bd014e3872291a579f69e
Katherine Paucar <gaby2091@outlook.es>
2023-12-09 13:49:10 +0100

    update table data

--- 1-React-optional/src/app.tsx
+++ 1-React-optional/src/app.tsx
@@ -3,9 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LoginPage } from "./login";
 import { ListPage } from "./list";
 import { DetailPage } from "./detail";
+import NavTabs from "./navBar";
+import ResponsiveAppBar from "./navBar";
 
 export const App = () => {
   return (
+    <>
+    <ResponsiveAppBar/>
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
@@ -13,5 +17,6 @@ export const App = () => {
         <Route path="/detail/:id" element={<DetailPage />} />
       </Routes>
     </Router>
+    </>
   );
 };
--- 1-React-optional/src/list.tsx
+++ 1-React-optional/src/list.tsx
@@ -2,12 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useDebounce } from "use-debounce";
 import { BasicPagination } from "./pagination";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 interface MemberEntity {
   id: string;
   login: string;
   avatar_url: string;
 }
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
 
 export const ListPage: React.FC = () => {
   const [members, setMembers] = React.useState<MemberEntity[]>([]);
@@ -30,15 +57,14 @@ export const ListPage: React.FC = () => {
       .then((res) => getData(res, newData.from, newData.to))
       .catch((err) => console.log(err));
   }, [sendData, newData.from, newData.to]);
-  const getData = (res: MemberEntity[], from: number, to:number) => {
+  const getData = (res: MemberEntity[], from: number, to: number) => {
     console.log("from", from);
     console.log("to", to);
-    if(res as MemberEntity[]){
+    if (res as MemberEntity[]) {
       const data = res.slice(from, to);
       setMembers(data);
       setTotalElemenst(res.length);
     }
-
   };
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -55,7 +81,7 @@ export const ListPage: React.FC = () => {
 
   return (
     <>
-      <h2>Hello from List page</h2>+{" "}
+      <h2>Hello from List page</h2>
       <form className="form" onSubmit={handleSubmit}>
         <input
           type="text"
@@ -65,20 +91,33 @@ export const ListPage: React.FC = () => {
         />
         <button>Filtrar por organización</button>
       </form>
-      <div className="list-user-list-container">
-        <span className="list-header">Avatar</span>
-        <span className="list-header">Id</span>
-        <span className="list-header">Name</span>
-        {members &&
-          members.length > 0 &&
-          members.map((member) => (
-            <React.Fragment key={member.id}>
-              <img src={member.avatar_url} />
-              <span>{member.id}</span>
-              <Link to={`/detail/${member.login}`}>{member.login}</Link>
-            </React.Fragment>
-          ))}
-      </div>
+      <TableContainer component={Paper}>
+        <Table sx={{ width: 600 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Avatar</StyledTableCell>
+              <StyledTableCell align="right">Id</StyledTableCell>
+              <StyledTableCell align="right">Name</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {members &&
+              members.length > 0 &&
+              members.map((member) => (
+                <StyledTableRow key={member.id}>
+                  <StyledTableCell component="th" scope="row">
+                    <img src={member.avatar_url} />
+                  </StyledTableCell>
+                  <StyledTableCell align="right">{member.id}</StyledTableCell>
+                  <StyledTableCell align="right">
+                    {" "}
+                    <Link to={`/detail/${member.login}`}>{member.login}</Link>
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
       <BasicPagination
         pageSize={perPage}
         totalElement={totalElement}
