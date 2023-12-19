0c7fad820665b45031e37ae02b8db77fffc8ea79
Katherine Paucar <gaby2091@outlook.es>
2023-12-19 19:37:03 +0100

    updata code

--- 2-React-basic-optional/src/list.tsx
+++ 2-React-basic-optional/src/list.tsx
@@ -1,77 +1,66 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useDebounce } from "use-debounce";
 import { Filter, MemberEntity, createEmptyFilter } from "./models";
 import { SearchContext } from "./context/search.context";
 import {
   Button,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
   TextField,
-  styled,
-  tableCellClasses,
 } from "@mui/material";
 import { BasicPagination } from "./pagination";
 import { MemberList } from "./member-list";
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
-}));
 
 export const ListPage: React.FC = () => {
   const { searchValue, setNewValue } = React.useContext(SearchContext);
-  const [members, setMembers] = React.useState<MemberEntity[]>([]);
   const [searchForm, setSearchForm] = React.useState<Filter>(
-    createEmptyFilter(searchValue)
-  );
-  const [debouncedSearchTerm] = useDebounce(searchForm, 500);
-  const [totalElement, setTotalElemenst] = React.useState<number>(0);
+                                    createEmptyFilter(searchValue)
+                                  );
+  const [members, setMembers] = React.useState<MemberEntity[]>([]);
+  const [error, setError] = React.useState(null);
   const perPage = 5;
   const defaultPage= 1;
-  const [resetP, setResetP] = React.useState<boolean>(false);
+  const [totalElement, setTotalElement] = React.useState<number>(0);
   const [newData, setNewData] = React.useState({
     from: 0,
     to: perPage,
   });
   React.useEffect(() => {
-    //https://api.github.com/orgs/${debouncedSearchTerm.org}/members
-    fetch(`https://api.github.com/orgs/${debouncedSearchTerm.org}/members`)
-      .then((response) => response.json())
+    //https://api.github.com/orgs/${searchForm.org}/members
+    fetch(`https://api.github.com/orgs/${searchForm.org}/members`)
+      .then(handleError)
       .then((res) => getData(res, newData.from, newData.to))
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        setError('Ha ocurrido un error');
+        setMembers([]);
+        setTotalElement(0);
+      });
   }, [searchValue, newData.from, newData.to]);
 
+  const handleError = response => {
+    if (!response.ok) { 
+        throw Error(response.statusText);
+    } else {
+        return response.json();
+    }
+  }; 
   const getData = (res: MemberEntity[], from: number, to: number) => {
     /*console.log("from", from);
     console.log("to", to);*/
     if (res as MemberEntity[]) {
       const data = res.slice(from, to);
       setMembers(data);
-      setTotalElemenst(res.length);
+      setTotalElement(res.length);
+      if(error){
+        setError(null);
+      }
+
     }
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setNewValue(debouncedSearchTerm.org);
+    setNewValue(searchForm.org);
   };
   const updateFieldValue =
     (name: keyof Filter) => (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -106,6 +95,7 @@ export const ListPage: React.FC = () => {
           </Button>
         </form>
         <MemberList members={members}></MemberList>
+        {error && <p className="text-error">{error}</p>}
         <BasicPagination
           pageSize={perPage}
           totalElement={totalElement}
