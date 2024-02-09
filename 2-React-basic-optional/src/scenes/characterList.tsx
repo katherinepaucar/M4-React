4b6ed4a26647476de1c9234012d697e885d2f4c9
Katherine Paucar <gaby2091@outlook.es>
2024-02-09 18:01:42 +0100

    update

--- 2-React-basic-optional/src/scenes/characterList.tsx
+++ 2-React-basic-optional/src/scenes/characterList.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 import { Layout } from "../layout/layout";
-import { SearchCharacterProvider } from "../core/rick-and-morty-context/characters.context";
 import { CharactersContainer } from "../pods/rickAndMorty-characters";
 
 
 export const CharacterListPage: React.FC = () => {
-  const { id } = useParams();
   
   return (
     <Layout>
--- 2-React-basic-optional/src/scenes/memberList.tsx
+++ 2-React-basic-optional/src/scenes/memberList.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Layout } from "../layout/layout";
-import { SearchMemberProvider } from "../core/github-members-context/search-member.context";
 import { MemberListContainer } from "../pods/github-members-list";
 
 export const MemberListPage: React.FC = () => {
